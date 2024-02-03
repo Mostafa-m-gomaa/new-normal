9273bfbc3c5d4fdf5d5565def76cc1115971c619
Mostafa-m-gomaa <aagomaaa2@gmail.com>
2024-02-03 20:29:42 +0200

    mostafa

--- src/components/DashboardSlide.jsx
+++ src/components/DashboardSlide.jsx
@@ -1,13 +1,13 @@
 import './dash.css'
-import one from "../assets/panars/aa.png";
+import one from "../assets/nnn.jpg";
 const DashboardSlide = () => {
   return (
     <div className="img-cont">
       <img src={one} className=" md:m-0 aspect-[3/1]" alt="" />
-      <h2 className='at-image-cont'>ابدأ رحلتك التعليمية</h2>
+      {/* <h2 className='at-image-cont'>ابدأ رحلتك التعليمية</h2>
       <h4 className='in-image-cont'>خبرات مختلفة أمتدت إلى 20 دولة حول العالم بهدف التعليم الذاتي في مجال التداول ونشر ثقافة الوعي المالي.
 
-</h4>
+</h4> */}
       {/* <button>ابدأ معنا</button> */}
     </div>
   );
